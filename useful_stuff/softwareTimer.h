235acf467dad50156be6ccca2b9574a6a8da7927
Michael Levy <mlevy94@vt.edu>
2016-02-01 19:38:57 -0500

    created folder with stuff that's useful on multiple projects.

---
+++ useful_stuff/softwareTimer.h
@@ -0,0 +1,33 @@
+/* 
+ * File:   timerCallback.h
+ * Author: Jacky
+ *
+ * Created on January 27, 2016, 6:10 PM
+ */
+
+#ifndef TIMERCALLBACK_H
+#define	TIMERCALLBACK_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "system_config.h"
+#include "system_definitions.h"
+
+#include <timers.h>
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    void initializeTimer();
+    void addToTimerFromQ(char* val);
+    BaseType_t addToTImerQISR(char* val);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TIMERCALLBACK_H */
+
