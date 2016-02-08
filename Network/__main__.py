b7feb52a1d024778e4f1a6c697a73e5dfe31f98e
Michael Levy <mlevy94@vt.edu>
2016-02-08 16:32:55 -0500

    Update to networks. Another attempt at fixing .gitignore

--- Network/__main__.py
+++ Network/__main__.py
@@ -2,6 +2,8 @@ from socket import socket
 from configs import getIPAddr, getPort
 from outbound import OutboundWorker
 from inbound import InboundWorker
+from threading import Thread
+from time import sleep
 
 class Connector:
 
@@ -13,19 +15,31 @@ class Connector:
 
   def start(self):
     self.outbound.start()
-    self.listener()
+    listenThread = Thread(target=self.listener, daemon=True)
+    listenThread.start()
+    cmdString = ""
+    sleep(0.5)
+    try:
+      while cmdString.lower() != "shutdown":
+        cmdString = input("Message: ")
+        self.queue.put(cmdString.encode() + b'\0')
+    except KeyboardInterrupt:
+      pass
 
   def listener(self):
     listener = socket()
     listener.bind((getIPAddr(), getPort()))
     listener.listen()
     print("Listening on {}:{}".format(getIPAddr(),getPort()))
-    while 1:
-      newSocket, addr = listener.accept()
-      newClient = InboundWorker(newSocket, self.queue, addr)
-      newClient.start()
-      self.clientList.append(newClient)
-      print("Client connected from {}".format(addr))
+    try:
+      while 1:
+        newSocket, addr = listener.accept()
+        newClient = InboundWorker(newSocket, self.queue, addr)
+        newClient.start()
+        self.clientList.append(newClient)
+        print("Client connected from {}".format(addr))
+    except KeyboardInterrupt:
+      listener.close()
 
 mainObj = Connector()
 mainObj.start()
