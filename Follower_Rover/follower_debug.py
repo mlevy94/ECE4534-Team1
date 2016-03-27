46249cbd92f21833a18186fdc386c4e7ab63b773
ccox04 <ccox04@vt.edu>
2016-03-26 23:44:03 -0400

    Changes for follower

--- Follower_Rover/follower_debug.py
+++ Follower_Rover/follower_debug.py
@@ -11,8 +11,6 @@ from queue import Empty
 MOVE_COUNT_FOLLOWER = 0;
 DISTANCE_COUNT_FOLLOWER = 0;
 
-listener = Listener()
-
 def queuePeek(recv_queue_data):
   print("Message : ", recv_queue_data)
   listener.queue.put(recv_queue_data)
@@ -45,75 +43,57 @@ def followerTokenFND():
 def scanServo():
   print("Servo Completed Scan")
 
-# This will eventually be used to write back to the PIC32
-def cmdInput(queue):
-  while 1:
-    msgstring = input().encode()
-    if msgstring == b'shutdown':
-      break
-    while msgstring:
-      msg = InternalMessage(ROUTER, DEBUG_MSG, msgstring[:INTERNAL_MSG_SIZE + 1])
-      queue.put(msg)
-      msgstring = msgstring[INTERNAL_MSG_SIZE + 1:]
-
-def incoming_message_handle(listener):
+def main():
+  listener = Listener()
+  #incoming_message_Thread = Thread(target=incoming_message_handle, args=[listener.clientList], daemon=True)
+  listener.start()
+  #incoming_message_Thread.start()
   while True:
     try:
       recv_queue_data = listener.queue.get(timeout=1)
       print("Message : ", recv_queue_data)
     except Empty:
+      print("empty")
       pass
-    try:
-      if listener.queue.empty():
-        print("empty")
-        pass
+  try:
+      recv_data = recv_queue_data.msgtype
+      message_pack_recv = recv_queue_data.msg
+      if recv_data == "Rover Move": # Follower to move FORWARD
+        if message_pack_recv == ROVER_FORWARD:
+          #totalMoves(distance)
+          print ("Received Follower move forward")
+        elif message_pack_recv == ROVER_BACKWARD: # Follower to move BACKWARD
+          #totalMoves(distance)
+          print ("Received Follower move backward")
+        elif message_pack_recv == ROVER_LEFT: # Follower to move LEFT
+          #totalMoves(distance)
+          print ("Received Follower move left")
+        elif message_pack_recv == ROVER_RIGHT: # Follower to move RIGHT
+          #totalMoves(distance)
+          print ("Received Follower move right")
+        elif message_pack_recv == ROVER_STOP: # Follower to STOP
+          #totalMoves(distance)
+          print ("Received Follower STOP")
+      elif recv_data == "Follower Distance": # Follower reports distance traveled
+        totalMoves(message_pack_recv)
+        print ("Received Follower Distance: ", message_pack_recv)
+      elif recv_data == "Follower Token Found": # Follower reports found token
+        followerTokenFND()
+      elif recv_data == "Scanning Follower Servo": # Scanning Follower Servo
+        if message_pack_recv == SCAN_STARTED: # Servo Scan Started
+          print ("Received Scanning Beginning")
+        elif message_pack_recv == LEAD_FOUND: # Scan returned Lead Found
+          scanServo()
+        elif message_pack_recv == OBJECT_FOUND: # Scan returned Object Found
+          scanServo()
+      elif recv_data == "Follower Distance To Lead": # Follower reports found token
+        print ("Received Follower Distance To Lead: ", message_pack_recv)
       else:
-        recv_data = recv_queue_data.msgtype
-        message_pack_recv = recv_queue_data.msg
-        if recv_data == "Rover Move": # Follower to move FORWARD
-          if message_pack_recv == ROVER_FORWARD:
-            #totalMoves(distance)
-            print ("Received Follower move forward")
-          elif message_pack_recv == ROVER_BACKWARD: # Follower to move BACKWARD
-            #totalMoves(distance)
-            print ("Received Follower move backward")
-          elif message_pack_recv == ROVER_LEFT: # Follower to move LEFT
-            #totalMoves(distance)
-            print ("Received Follower move left")
-          elif message_pack_recv == ROVER_RIGHT: # Follower to move RIGHT
-            #totalMoves(distance)
-            print ("Received Follower move right")
-          elif message_pack_recv == ROVER_STOP: # Follower to STOP
-            #totalMoves(distance)
-            print ("Received Follower STOP")
-        elif recv_data == "Follower Distance": # Follower reports distance traveled
-          totalMoves(message_pack_recv)
-          print ("Received Follower Distance: ", message_pack_recv)
-        elif recv_data == "Follower Token Found": # Follower reports found token
-          followerTokenFND()
-        elif recv_data == "Scanning Follower Servo": # Scanning Follower Servo
-          if message_pack_recv == SCAN_STARTED: # Servo Scan Started
-            print ("Received Scanning Beginning")
-          elif message_pack_recv == LEAD_FOUND: # Scan returned Lead Found
-            scanServo()
-          elif message_pack_recv == OBJECT_FOUND: # Scan returned Object Found
-            scanServo()
-        elif recv_data == "Follower Distance To Lead": # Follower reports found token
-          print ("Received Follower Distance To Lead: ", message_pack_recv)
-        else:
-          # Something unexpected happened
-          print("Received unexpected data %d" % recv_data)
-    except KeyboardInterrupt:
-      print("CTRL C Detected")
-      return
-
-
-def main():
-  incoming_message_Thread = Thread(target=incoming_message_handle, args=[listener], daemon=True)
-  listener.start()
-  incoming_message_Thread.start()
-  while incoming_message_Thread.is_alive(): print("True")
-  
+        # Something unexpected happened
+        print("Received unexpected data %d" % recv_data)
+  except KeyboardInterrupt:
+    print("CTRL C Detected")
+    return
   listener.close()
 
 if __name__ == "__main__":
